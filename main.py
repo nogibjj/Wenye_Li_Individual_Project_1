56b2121f4250f38277c6a4ae717358428fabc721
Wenye Li <wl275@duke.edu>
2024-09-20 04:10:27 -0400

    Save dataset statistics and visualizations to Markdown and PNG files

--- main.py
+++ main.py
@@ -5,23 +5,32 @@ Main cli or app entry point
 from mylib.lib import *
 
 def general_describe():
-    # Save dataset statistics to Markdown
+    """Save dataset statistics to Markdown and return the description."""
     with open('test.md', 'w') as file:
         description = describe_dataset()
         file.write("# Dataset Statistics\n")
-        file.write(description.to_markdown())  # Convert DataFrame to Markdown format
+        file.write(description.to_markdown())
     return description
 
 def general_visualize():
-    # Save visualizations to PNG files
+    """Generate and save visualizations as PNG files."""
     create_histogram("alcohol_use_histogram.png")
     create_line_chart("marijuana_use_line_chart.png")
     create_bar_chart("cocaine_use_bar_chart.png")
 
 def save_to_markdown():
+    """Append visualizations and other information to Markdown."""
     with open('test.md', 'a') as file:
-        file.write("## Test section\n")
-        file.write("This is a test entry in the markdown file.")
+        # Append images to the Markdown
+        file.write("\n## Visualizations\n")
+        file.write("### Alcohol Use Histogram\n")
+        file.write("![Alcohol Use](alcohol_use_histogram.png)\n")
+        
+        file.write("### Marijuana Use Line Chart\n")
+        file.write("![Marijuana Use](marijuana_use_line_chart.png)\n")
+        
+        file.write("### Cocaine Use Bar Chart\n")
+        file.write("![Cocaine Use](cocaine_use_bar_chart.png)\n")
 
 if __name__ == '__main__':
     print(general_describe())
